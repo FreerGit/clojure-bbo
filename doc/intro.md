4cb857b25f933057aa555dfe81e2a58fada0450b
root <root@DESKTOP-F2TLKQA>
2023-10-22 19:59:40 +0200

    chore: scaffold

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to freergit/clojure-bbo
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/freergit/clojure_bbo_test.clj
@@ -0,0 +1,7 @@
+(ns freergit.clojure-bbo-test
+  (:require [clojure.test :refer :all]
+            [freergit.clojure-bbo :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
